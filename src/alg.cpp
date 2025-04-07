47c0d10cc7f920375e3c2b65eb063bed9b915f55
z1g-zak <horevdaniil54@gmail.com>
2025-04-07 15:21:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,45 +2,72 @@
 #include <unordered_map>
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-for (int i = 0; i < len; ++i) {
+  for (int i = 0; i < len - 1; ++i) {
     for (int j = i + 1; j < len; ++j) {
-        if (arr[i] + arr[j] == value) {
-            count++;
-        }
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
     }
-}
-return count;
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0, left = 0, right = len - 1;
-
-while (left < right) {
-	int sum = arr[left] + arr[right];
-	if (sum == value) {
-		count++;
-		right--;
-	}
-	else if (sum < value) {
-		left++;
-	}
-	else {
-		right--;
-	}
+  int count = 0;
+  int j = len - 1, fl = 0, point = len - 1;
+  for (int i = 0; i < len - 1; ++i) {
+    j = point;
+    while (i < j) {
+      int sum = arr[i] + arr[j];
+      if (sum == value) {
+        count++;
+        if (fl == 1) {
+          point = j;
+          fl = 0;
+        }
+      }
+      else if (sum < value) {
+        break;
+      }
+      else {
+        fl = 1;
+      }
+      j--;
+    }
+  }
+  return count;
 }
-
-return count;
+int BinarySearch(int* arr, int left, int right, int value) {
+  int first = -1, _left = left, _right = right;
+  while (_left <= _right) {
+    int mid = _left + (_right - _left) / 2;
+    if (arr[mid] >= value) {
+      _right = mid - 1;
+      if (arr[mid] == value) first = mid;
+    }
+    else {
+      _left = mid + 1;
+    }
+  }
+  if (first == -1) return 0;
+  int last = first;
+  _left = first;
+  _right = right;
+  while (_left <= _right) {
+    int mid = _left + (_right - _left) / 2;
+    if (arr[mid] <= value) {
+      _left = mid + 1;
+      if (arr[mid] == value) last = mid;
+    }
+    else {
+      _right = mid - 1;
+    }
+  }
+  return last - first + 1;
 }
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-std::unordered_map<int, int> num_counter;
-for (int i = 0; i < len; ++i) {
-    num_counter[arr[i]]++;
-}
-for (int i = 0; i < len; ++i) {
-    int complement = value - arr[i];
-    if (num_counter.count(complement)&& complement > arr[i]) {
-        count += num_counter[complement];
-    }
-}
-return count;
+  for (int i = 0; i < len; ++i) {
+    count += BinarySearch(arr, i + 1, len - 1, value - arr[i]);
+  }
+  return count;
 }
